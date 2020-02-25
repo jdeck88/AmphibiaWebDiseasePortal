a7fac15f432318aae71ce4524fb3e27bfafff957
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-02-24 16:28:08 -0800

    add detail buttons to project table.

--- static/js/projects.js
+++ static/js/projects.js
@@ -15,31 +15,6 @@ function downloadButton(a) {
 	})
 }
 
-// TODO: Fix the toggle so it only toggles relevant data.
-
-  // Toggle data
-  var coll = document.getElementsByClassName("collapsible");
-
-  function toggle() {
-    for (let i = 0; i < coll.length; i++) {
-        if (coll[i].style.display === "block") {
-          coll[i].style.display = "none";
-        } else {
-          coll[i].style.display = "block";
-        }
-      }
-  }
-
-
-function createNode(element) {
-  return document.createElement(element)
-}
-
-function append(parent, el) {
-  return parent.appendChild(el)
-}
-
-let dl = document.getElementById('projects-display')
 
 // Fetching all projects from GEOME
 let baseURL = 'https://api.geome-db.org/projects/stats?'
@@ -54,17 +29,54 @@ function fetchProjects() {
       if(project.projectConfiguration.id == 45 && project.public == true) {
         //console.log(project)
 
-        var table = document.getElementById('projects-display')
-        var tr = document.createElement('tr');
+        const table = document.getElementById('projects-display')
+        let tr = document.createElement('tr') // Table row
+        let p = document.createElement('p') // Paragraph tag
+        let modalContent = document.getElementById('m-content')
+
 
                 tr.innerHTML = `
                 <td> <i class="fa fa-globe"></i> </td>
                 <td> ${project.projectTitle} </td>
                 <td> ${project.principalInvestigator} </td>
                 <td> ${project.principalInvestigatorAffiliation} </td>
+                <td><button id="detailsBtn" data-id='${project.projectId}'>Details</button></td>
                 `
-                table.appendChild(tr);
-      
+                table.appendChild(tr)
+
+                //TODO: Get all buttons to be clickable.
+                // Get each button to correspond to each project and be able to 
+                // display the data for that project.
+                
+                p.innerHTML =`
+                ${project.projectTitle}
+                `
+                modalContent.appendChild(p)
+
+          // Get the button that opens the modal
+          let btn = document.getElementById("detailsBtn");
+
+          // Get the modal
+          let modal = document.getElementById("detailModal");
+
+          // Get the <span> element that closes the modal
+          let span = document.getElementsByClassName("close")[0];
+          // When the user clicks on the button, open the modal
+          btn.onclick = function() {
+            modal.style.display = "block";
+          }
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function() {
+            modal.style.display = "none";
+          }
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          }
+
+
       }
     })
   })
