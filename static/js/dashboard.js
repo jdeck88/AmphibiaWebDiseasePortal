163c88501a1bb4a65648e448893c976c82a975f8
John Deck <jdeck88@gmail.com>
2020-03-25 08:32:24 -0700

    making projectIds variable in dashboard reference class-level scope.  changed the example to a public project that works

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -7,7 +7,7 @@ class Dashboard{
       // TODO: create method to select projects that are part of the AD portal team (see /project/stats JSON call)
       // For now, the uploaded project identifiers are hard-coded right here, see
       // https://github.com/jdeck88/AmphibiaWebDiseasePortal/issues/6
-      var projectIds = "221,222,223"
+      this.projectIds = "255"
   
       var dashboardForm = $('#dashboardForm');
       var dashboardSelect = $('<select>').appendTo(dashboardForm);
@@ -35,7 +35,7 @@ class Dashboard{
     // Count of records by country
     countryCount() {
       d3
-      .json("https://api.geome-db.org/records/Sample/json?limit=10000&page=0&networkId=1&q=_projects_:" + projectIds + "+" +
+      .json("https://api.geome-db.org/records/Sample/json?limit=10000&page=0&networkId=1&q=_projects_:" + this.projectIds + "+" +
           "_select_:%5BEvent%5D+&source=Event.country,expeditionCode")
       .then(function(samples) {
         var metrics = d3.nest()
@@ -61,7 +61,7 @@ class Dashboard{
     // Count of records by yearCollected
     yearCollectedCount() {
       d3
-      .json("https://api.geome-db.org/records/Sample/json?limit=10000&page=0&networkId=1&q=_projects_:" + projectIds +"+" +
+      .json("https://api.geome-db.org/records/Sample/json?limit=10000&page=0&networkId=1&q=_projects_:" + this.projectIds +"+" +
           "_select_:%5BEvent%5D+&source=Event.yearCollected,expeditionCode")
       .then(function(samples) {
         var metrics = d3.nest()
