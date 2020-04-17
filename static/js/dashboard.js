922df0c06d2de1282b0f54cc826d1e145695c6c1
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-04-16 18:11:18 -0700

    start dash

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,14 +1,14 @@
 class Dashboard{
     // Build the dashboard chart interface
     constructor() {
-      console.log("Creating dashboard")
       var mychart = this;
   
       // TODO: create method to select projects that are part of the AD portal team (see /project/stats JSON call)
       // For now, the uploaded project identifiers are hard-coded right here, see
       // https://github.com/jdeck88/AmphibiaWebDiseasePortal/issues/6
       this.projectIds = "255"
-  
+      
+      // First dropdown
       var dashboardForm = $('#dashboardForm');
       var dashboardSelect = $('<select>').appendTo(dashboardForm);
   
@@ -16,18 +16,65 @@ class Dashboard{
       dashboardSelect.append($("<option>").attr("value","").text("-- Choose a Value --"));
       dashboardSelect.append($("<option>").attr("value","country").text("Country Count"));
       dashboardSelect.append($("<option>").attr("value","yearCollected").text("Year Collected Count"));
+      dashboardSelect.append($("<option>").attr("value","bdByCountry").text("Bd Counts By Country"));
+
   
       dashboardForm.append(document.createTextNode("Choose a chart:"));
       dashboardForm.append(dashboardSelect);
-  
-      //dashboardSelect.setAttribute("onclick","drawChart(dashboardSelect.value);")
+
       $('#dashboardSelect').change(function(mychart) {
         var selectedVariable = $('#dashboardSelect').val().trim()
         if (selectedVariable == "country") {
           dashboard.countryCount();
-        } else {
+        } else if (selectedVariable == "yearCollected"){
           dashboard.yearCollectedCount();
+        } else if ((selectedVariable == "bdByCountry")) {
+          dashboard.bdByCountry()
         }
+      })
+
+      // project Dropdown
+      let projectDash = $('#project-dash')
+      let projectDashSelect = $('<select>').appendTo(projectDash)
+
+      projectDashSelect.attr('id', 'projectDashSelect')
+      projectDashSelect.append($("<option>").attr("value","").text("-- Select By Project --"))
+      projectDashSelect.append($("<option>").attr("value","sample").text("Testing"))
+
+      projectDash.append(document.createTextNode('View Data By Individual Project:'))
+      projectDash.append(projectDashSelect)
+
+      document.querySelector('#projectDashSelect').addEventListener('change', function() {
+        let selectedOption = document.querySelector('#projectDashSelect').value
+        
+        if (selectedOption == 'sample') {
+          console.log(selectedOption)
+        }
+      })
+
+    }
+
+
+    bdByCountry() {
+      d3.json('https://raw.githubusercontent.com/BNHM/AmphibiaWebDiseasePortalAPI/master/data/country_Bd.json')
+      .then(samples => {
+        //console.log(samples)
+        let data = d3.nest()
+        .key(function(d) { return (d.country) })
+        .rollup(function(v) { return d3.sum(v, function(d) { return d.value })})
+        .entries(samples)
+    
+        //console.log(data)
+
+        var labels = data.map(function(d) {
+          console.log(d.key)
+          return d.key
+        })
+        var codes = data.map(function(d) {
+          console.log(d.value)
+          return d.value
+        })
+        dashboard.makeGenericChart(labels, codes)
       })
     }
     
